453918b81196e084df5ad47f1d15386162d62142
Sébastien Piquemal <sebpiq@gmail.com>
2013-08-12 01:01:52 +0400

    working-ish AudioParam

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var BUFFSIZE = 16384
 
-var AudioParam = window.AudioParam = function(context, defaultValue) {
-  var scriptNode = context.createScriptProcessor(0, 1, BUFFSIZE)
+var AudioParam = module.exports = window.AudioParam = function(context, defaultValue) {
+  var scriptNode = context.createScriptProcessor(BUFFSIZE, 0, 1)
     , gainNode = context.createGain()
     , outputBuffer = new Float32Array(BUFFSIZE)
-    , i
-
+    , i, audioParam
   for (i = 0; i < BUFFSIZE; i++) outputBuffer[i] = 1
 
+  // Generate ones to feed the gain
   scriptNode.onaudioprocess = function(event) {
     event.outputBuffer.getChannelData(0).set(outputBuffer)
   }
-
   scriptNode.connect(gainNode)
-  gainNode.gain.value = defaultValue
 
-  return gainNode.gain
+  // The AudioParam
+  audioParam = gainNode.gain
+  audioParam.value = defaultValue
+  audioParam.connect = (function(gainNode, scriptNode) {
+    return function(destination, input) {
+      gainNode.connect(destination, 0, input)
+    }
+  })(gainNode, scriptNode)
+  return audioParam
 }
